ec5b011533f45206b31089c3c1456b768b04c3a2
rohitmathur-7 <rohitmathur.mathur.6@gmail.com>
2024-12-18 11:14:27 +0530

    Add constants file

---
+++ src/utils/constants.js
@@ -0,0 +1,26 @@
+export const FIREBASE_API = process.env.REACT_APP_FIREBASE_API;
+export const LOGO =
+  "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
+
+export const PROFILE_IMG =
+  "https://occ-0-6546-3647.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABTZ2zlLdBVC05fsd2YQAR43J6vB1NAUBOOrxt7oaFATxMhtdzlNZ846H3D8TZzooe2-FT853YVYs8p001KVFYopWi4D4NXM.png?r=229";
+
+export const HOME_BG =
+  "https://assets.nflxext.com/ffe/siteui/vlv3/b2c3e95b-b7b5-4bb7-a883-f4bfc7472fb7/19fc1a4c-82db-4481-ad08-3a1dffbb8c39/IN-en-20240805-POP_SIGNUP_TWO_WEEKS-perspective_WEB_24a485f6-1820-42be-9b60-1b066f1eb869_large.jpg";
+
+export const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: "Bearer " + process.env.REACT_APP_TMDB_API,
+  },
+};
+
+export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w780";
+
+export const SUPPORTED_LANGS = [
+  { identifier: "en", name: "English" },
+  { identifier: "hi", name: "Hindi" },
+];
+
+export const GEMINI_API_KEY = process.env.REACT_APP_GEMINI_API;
