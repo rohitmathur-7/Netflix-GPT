248ee140bf12e8f2c086a893eaa0cfc28ec359b7
rohitmathur-7 <rohitmathur.mathur.6@gmail.com>
2025-02-09 12:45:50 +0530

    Made the movie slider responsive

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -16,6 +16,22 @@ const MovieList = ({ title, movies, isGptMovies, lastList = false }) => {
 		speed: 500,
 		slidesToShow: 5,
 		slidesToScroll: 5,
+		responsive: [
+			{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 3,
+				},
+			},
+			{
+				breakpoint: 600,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2,
+				},
+			},
+		],
 	};
 
 	return (
