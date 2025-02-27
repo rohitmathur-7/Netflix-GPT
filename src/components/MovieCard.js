f70dc450d9de5a95c681c8a1016031023439af20
rohitmathur-7 <rohitmathur.mathur.6@gmail.com>
2025-02-27 08:46:48 +0530

    chore: Fix the movie hover card animation

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,7 +1,7 @@
 import React, { useState, useRef, useEffect } from "react";
 import { IMG_CDN_URL } from "../utils/constants";
 import SingleMovie from "./SingleMovie";
-import { CiCirclePlus, CiCircleCheck } from "react-icons/ci"; // Import both icons
+import { CiCirclePlus, CiCircleCheck } from "react-icons/ci";
 import { IoPlayCircleOutline } from "react-icons/io5";
 import { movieGenres } from "../utils/movieGeneres";
 import { useDispatch, useSelector } from "react-redux";
@@ -22,7 +22,6 @@ const MovieCard = ({
 	const [isHover, setIsHover] = useState(false);
 	const cardRef = useRef(null);
 	const hoverTimeoutRef = useRef(null);
-	const [cardLeftPosition, setCardLeftPosition] = useState(0);
 	const [imgWidth, setImgWidth] = useState(0);
 	const movieCardImgRef = useRef(null);
 	const movieGenereIds = movie.genre_ids.slice(0, 2);
@@ -53,8 +52,15 @@ const MovieCard = ({
 
 	const handleMouseLeave = () => {
 		clearTimeout(hoverTimeoutRef.current);
-		setIsHover(false);
-		onHoverChange(false);
+		const hoverElement = cardRef.current.querySelector(".movie-card-hover");
+		if (hoverElement) {
+			hoverElement.classList.add("scale-down");
+			setTimeout(() => {
+				setIsHover(false);
+				onHoverChange(false);
+				hoverElement.classList.remove("scale-down");
+			}, 100);
+		}
 	};
 
 	const handleMouseLeaveMain = () => {
@@ -63,7 +69,6 @@ const MovieCard = ({
 
 	const handleWishlistToggle = (e) => {
 		e.preventDefault();
-
 		if (isMovieInWishlist) {
 			// If the movie is in the wishlist, remove it
 			dispatch(removeFromWishlist(movie.id));
@@ -74,9 +79,9 @@ const MovieCard = ({
 	};
 
 	return !showSingleMovie ? (
-		<div ref={cardRef}>
+		<div ref={cardRef} className="relative">
 			<div
-				className={`movie-card h-full cursor-pointer relative z-10`}
+				className="movie-card h-full cursor-pointer relative z-10"
 				onMouseEnter={handleMouseEnter}
 				onMouseLeave={handleMouseLeaveMain}
 				onClick={handleMovieClick}
@@ -84,18 +89,20 @@ const MovieCard = ({
 				<img
 					alt="Movie Card"
 					src={IMG_CDN_URL + posterPath}
-					className="rounded-md"
+					className="rounded-md rounded-b-none"
 					ref={movieCardImgRef}
 				/>
 			</div>
 			{isHover && (
 				<div
-					className={`movie-card-hover cursor-pointer absolute z-[100] transition-all duration-300 ease-in-out`}
+					className="movie-card-hover cursor-pointer absolute z-[100] transition-all duration-300 ease-in-out"
 					style={{
-						top: lastList ? "-8%" : "0",
-						transform: `translateX(${
-							firstMovie ? "45px" : lastMovie ? "-45px" : "0"
-						}) scale(1)`,
+						top: lastList ? "-3%" : "0",
+						transformOrigin: firstMovie
+							? "left"
+							: lastMovie
+							? "right"
+							: "center",
 					}}
 					onMouseLeave={handleMouseLeave}
 				>
@@ -103,12 +110,12 @@ const MovieCard = ({
 						<img
 							alt="Movie Card"
 							src={IMG_CDN_URL + posterPath}
-							className="rounded-md"
+							className="rounded-md rounded-b-none"
 							style={{
 								width: imgWidth,
 							}}
 						/>
-						<div className="flex flex-col gap-2 bg-[#141414] text-white p-4">
+						<div className="flex flex-col gap-2 bg-[#141414] text-white p-4 rounded-md rounded-t-none">
 							<div className="flex">
 								<IoPlayCircleOutline
 									size={24}
