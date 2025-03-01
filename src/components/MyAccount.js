14722549f18ff0f23564fc2323b4a4d6e760de51
rohitmathur-7 <rohitmathur.mathur.6@gmail.com>
2025-03-01 17:58:20 +0530

    chore: Upate my account

--- src/components/MyAccount.js
+++ src/components/MyAccount.js
@@ -1,22 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import user from "../assets/images/user.jpg";
 import { useSelector } from "react-redux";
 
 const MyAccount = ({ showAccountMenu, setShowAccountMenu, handleSignOut }) => {
+	const [showUnderline, setShowUnderline] = useState(false);
 	const userName = useSelector((store) => store.user.displayName);
 
 	return (
-		<div className="user-details-container absolute flex flex-col justify-center px-4 py-4 opacity-90 gap-4 min-w-[120px] top-[50px] right-0 bg-black">
-			<div className="user-details flex flex-wrap items-center justify-center gap-2">
+		<div className="user-details-container absolute flex flex-col justify-center items-center pt-4 opacity-100 gap-4 min-w-[150px] top-[50px] right-0 bg-black z-[10]">
+			<div className="user-details flex flex-wrap items-center justify-center gap-2 px-4">
 				<img className="w-8 rounded" src={user} alt="User Profile Image" />
 				<span>{userName}</span>
 			</div>
-			<button
-				className="whitespace-nowrap underline cursor-pointer"
-				onClick={handleSignOut}
-			>
-				Sign Out
-			</button>{" "}
+			<div className="w-full flex items-center justify-center border-t-[1px] py-4">
+				<button
+					className="relative whitespace-nowrap cursor-pointer w-fit"
+					onClick={handleSignOut}
+					onMouseEnter={() => setShowUnderline(true)}
+					onMouseLeave={() => setShowUnderline(false)}
+				>
+					Sign Out
+					<span
+						className={`absolute -bottom-1 left-0 h-0.5 bg-white transition-all duration-100 ${
+							showUnderline ? "w-full" : "w-0"
+						}`}
+					></span>
+				</button>
+			</div>
 		</div>
 	);
 };
